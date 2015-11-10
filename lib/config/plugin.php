0177e945a73ae3fa14a4e0ea27b6227fc542f842
korob <korob@192.168.1.180>
2015-11-10 17:00:09 +0500

    настройка маски для динамически появляющегося контента

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'Маска для телефона',
     'description' => 'Ввод телефона по маске заданного формата',
     'vendor' => '985310',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'img' => 'img/phmask.png',
     'shop_settings' => true,
     'frontend' => true,
